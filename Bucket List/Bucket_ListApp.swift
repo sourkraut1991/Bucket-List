92364a1923213456ca0a601274f064618b55b4ce
sourkraut1991 <69095925+sourkraut1991@users.noreply.github.com>
2023-03-11 09:30:56 +0300

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Ed Kraus on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
